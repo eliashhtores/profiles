72e2f6d545a76ce48759d9f8e222e31a1baaaa22
eliashhtorres <eliashhtorres@gmail.com>
2022-04-05 07:21:00 -0500

    Added user profile module and manager

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'is_staff')
+    search_fields = ('email', 'name')
+    list_fields = ('email', 'name')
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- api/models.py
+++ api/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .models import UserProfileManager
 
 
-class UserProfile(AbstractBaseUser):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = models.Manager()
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
